5b1c045c9d7988fa9da1a63f085b110c171992e6
Jeff Becca <jeff.becca@gmail.com>
2019-08-14 00:05:56 -0400

    created unit test file

---
+++ test-chem-package.py
@@ -0,0 +1,13 @@
+import unittest
+
+class TestFileType(unittest.TestCase):
+
+    def test_inputOrOutput(self):
+        #a user wants to manipulate information in a file, so first the file is supplied
+        #self.assertEqual
+
+    def test_input(self):
+        #self.assertEqual('foo'.upper(), 'FOO')
+
+if __name__ == '__main__':
+    unittest.main()
